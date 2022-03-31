840986acc1e0c37818f3038aa36f88bf5c78d932
Marius <marius@example.com>
2022-03-31 11:56:12 +0300

    iinerHtml

---
+++ js/modules/data.js
@@ -0,0 +1,2 @@
+export const p =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dicta quis pariatur praesentium quasi reprehenderit quidem ut quae aliquid earum. <button>click me</button>';
