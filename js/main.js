723743a6cc1bb4917aeb0f44c40f8a25f1ce263d
Marius <marius@example.com>
2022-03-31 11:33:41 +0300

    avg, count uzd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+import { countAvg, countAvgThree } from './modules/avg.js';
+// import * as avg from './modules/avg.js';
+
 console.log('main');
+const appEl = document.getElementById('app');
+
+// sukurti funkcija kuri priima sk argumenta. padvigubina argumenta ir prideda 10 ir grazina
+// perkelti makeDoubleAndPlus i count.js ir export import
+function makeDoubleAndPlus(num) {
+  const double = num * 2;
+  const plus10 = double + 10;
+  return plus10;
+}
+
+const nr1 = makeDoubleAndPlus(10);
+console.log('nr1 ===', nr1); // istestuoti ar veikia
+const nr2 = makeDoubleAndPlus(100);
+console.log('nr2 ===', nr2); // istestuoti ar veikia
+
+const nr3 = countAvg(100, 150);
+console.log('nr3 ===', nr3); // istestuoti ar veikia
+const nr4 = countAvgThree(100, 150, 250);
+console.log('nr4 ===', nr4); // istestuoti ar veikia
+
+// sukurti funkcija kuri ima 2 argumenus. pirmas argumentas yra reiksme, ntras argumentas yra el html elementas kur ta reiksme atspaudinti html
+// takeAndPrint(500, appEl)
+// makeLiAndAppend(obj, appEl)
---
+++ js/modules/avg.js
@@ -0,0 +1,8 @@
+// sukurti funkcija kuri grazina dvieju paduotu skaiciu vidurki // countAvg(10, 15)
+export function countAvg(num1, num2) {
+  return (num1 + num2) / 2;
+}
+
+export function countAvgThree(num1, num2, num3) {
+  return (num1 + num2 + num3) / 3;
+}
